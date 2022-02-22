f1130ee3ae9fca9ed7b55f103413b5103cf58367
xuepeng <xuepeng@chinasofti.com>
2022-02-22 20:03:42 +0800

    add ad

--- Fabmedical/content-web/src/app/app.module.ts
+++ Fabmedical/content-web/src/app/app.module.ts
@@ -7,9 +7,64 @@ import { HomeComponent } from './home/home.component';
 import { SpeakersComponent } from './speakers/speakers.component';
 import { SessionsComponent } from './sessions/sessions.component';
 import { StatsComponent } from './stats/stats.component';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+
+import { HttpClientModule, HttpClient ,HTTP_INTERCEPTORS} from '@angular/common/http';
+import { IPublicClientApplication, PublicClientApplication, InteractionType, BrowserCacheLocation, LogLevel } from '@azure/msal-browser';
+import { MsalGuard, MsalInterceptor, MsalBroadcastService, MsalInterceptorConfiguration, MsalModule, MsalService, MSAL_GUARD_CONFIG, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG, MsalGuardConfiguration, MsalRedirectComponent } from '@azure/msal-angular';
+const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
+
+
+
+
 import { WINDOW_PROVIDERS } from './window.provider';
 import { AppService } from './app.service';
+
+
+
+export function loggerCallback(logLevel: LogLevel, message: string) {
+  console.log(message);
+}
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: '7906a1e3-3aa2-426a-a7cc-80b4148d0722',
+      authority: 'https://login.microsoftonline.com/9349e29b-779b-4883-a118-b5be4d6dc66a',
+      redirectUri: 'http://localhost:4200/'
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage,
+      storeAuthStateInCookie: isIE, // set to true for IE 11
+    },
+    system: {
+      loggerOptions: {
+        loggerCallback,
+        logLevel: LogLevel.Info,
+        piiLoggingEnabled: false
+      }
+    }
+  });
+}
+
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+  protectedResourceMap.set('https://graph.microsoft.com/v1.0/me', ['user.read']);
+
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap
+  };
+}
+
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return { 
+    interactionType: InteractionType.Redirect,
+    authRequest: {
+      scopes: ['user.read']
+    }
+  };
+}
 
 export function initApp(appService: AppService) {
   return () => {
@@ -27,16 +82,39 @@ export function initApp(appService: AppService) {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MsalModule
   ],
-  providers: [WINDOW_PROVIDERS,
+  providers: [
+    WINDOW_PROVIDERS,
     AppService, {
       provide: APP_INITIALIZER,
       useFactory: initApp,
       multi: true,
       deps: [AppService]
-    }],
-  bootstrap: [AppComponent]
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService
+  ],
+  bootstrap: [AppComponent,MsalRedirectComponent]
 })
 export class AppModule {
 }
--- Fabmedical/content-web/src/app/home/home.component.ts
+++ Fabmedical/content-web/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EventMessage, EventType, AuthenticationResult } from '@azure/msal-browser';
+import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  loginDisplay = false;
 
-  constructor() { }
+  constructor(private authService: MsalService, private msalBroadcastService: MsalBroadcastService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.msalBroadcastService.msalSubject$
+      .pipe(
+        filter((msg: EventMessage) => msg.eventType === EventType.LOGIN_SUCCESS),
+      )
+      .subscribe((result: EventMessage) => {
+        console.log(result);
+        const payload = result.payload as AuthenticationResult;
+        this.authService.instance.setActiveAccount(payload.account);
+      });
+
+      this.setLoginDisplay();
+  }
+
+  setLoginDisplay() {
+    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
   }
 
 }
